ed0b203f7c7b5226f14c56279db60f9cfcb16fb1
tianyu <2934301436@qq.com>
2019-06-21 14:51:36 +0800

    1

---
+++ src/com/hik/java/Hello.java
@@ -0,0 +1,11 @@
+package com.hik.java;
+
+/**
+ * @author Tianxiangyu
+ * @create 2019-06-21 下午2:49
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
